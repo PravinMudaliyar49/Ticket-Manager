f55d276bc328e992f1b56dc8406199d22fdcb3b7
Pravin <pravincoding49@gmail.com>
2021-11-24 19:56:30 +0530

    Implemented the lock feature

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
 let addBtn = document.querySelector(".add-btn");
+let removeBtn = document.querySelector(".remove-btn");
+
 let modalCont = document.querySelector(".modal-cont");
 let mainCont = document.querySelector(".main-cont");
 let taskAreaCont = document.querySelector(".textarea-cont");
 let allPriorityColors = document.querySelectorAll(".priority-color");
 
 let addModal = true;
+let removeFlag = true;
+
+let lockClass = "fa-lock";
+let unLockClass = "fa-unlock"
 
 let colors = ["lightpink", "lightblue", "lightgreen", "black"];
 let modalPriorityColor = colors[colors.length - 1];
@@ -37,6 +43,10 @@ addBtn.addEventListener("click", (event) => {
     addModal = !addModal;
 });
 
+removeBtn.addEventListener("click", (event) => {
+    removeFlag = !removeFlag;
+});
+
 modalCont.addEventListener("keydown", (event) => {
     let key = event.key;
     if (key === 'Shift') {
@@ -55,11 +65,41 @@ function createTicket(ticketColor, ticketTask, ticketId) {
     <div class="ticket-color ${ticketColor} "></div>
     <div class="ticket-id">${ticketId}</div>
     <div class="task-area">${ticketTask}</div>
+    <div class="ticket-lock">
+    <i class="fas fa-lock"></i>
+    </div>
     `;
 
     mainCont.append(ticketCont);
 
+    handleRemoval(ticketCont);
+    handleLock(ticketCont);
 }
+
+function handleRemoval(ticket) {
+    if (removeFlag) {
+        ticket.remove();
+    }
+}
+
+function handleLock(ticket) {
+    let ticketLockElem = ticket.querySelector(".ticket-lock");
+    let ticketLock = ticketLockElem.children[0];
+    let ticketTaskArea = ticket.querySelector(".task-area");
+
+    ticketLock.addEventListener("click", (event) => {
+        if (ticketLock.classList.contains(lockClass)) {
+            ticketLock.classList.remove(lockClass);
+            ticketLock.classList.add(unLockClass);
+            ticketTaskArea.setAttribute("contenteditable", true);
+        } else {
+            ticketLock.classList.remove(unLockClass);
+            ticketLock.classList.add(lockClass);
+            ticketTaskArea.setAttribute("contenteditable", false);
+        }
+    });
+}
+
 
 
 
